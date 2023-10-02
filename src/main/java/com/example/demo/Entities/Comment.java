799f1c136130d8e204eca8974b687475ccc78b0f
marc <fmarcandrei@gmail.com>
2023-10-02 19:29:02 +0300

    initial

--- src/main/java/com/example/demo/Entities/Comment.java
+++ src/main/java/com/example/demo/Entities/Comment.java
@@ -1,4 +1,8 @@
 package com.example.demo.Entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.Instant;
 public class Comment {
 }
--- src/main/java/com/example/demo/Entities/User.java
+++ src/main/java/com/example/demo/Entities/User.java
@@ -1,4 +1,18 @@
 package com.example.demo.Entities;
+import jakarta.persistence.*;
+import lombok.*;
 
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
 }
