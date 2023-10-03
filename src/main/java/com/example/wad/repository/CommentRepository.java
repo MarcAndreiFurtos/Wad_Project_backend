6c6fae1d6b1be447000791ced73c499cb2d52a82
marc <fmarcandrei@gmail.com>
2023-10-03 16:42:48 +0300

    added CommentRepository

---
+++ src/main/java/com/example/wad/repository/CommentRepository.java
@@ -0,0 +1,16 @@
+package com.example.wad.repository;
+
+import com.example.wad.Entities.Comment;
+import com.example.wad.Entities.Post;
+import com.example.wad.Entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPost(Post post);
+
+    List<Comment> findAllByUser(User user);
+}
